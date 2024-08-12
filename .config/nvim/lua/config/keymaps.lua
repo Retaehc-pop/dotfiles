13333075715f46325333d8e9de05e107d2210f68
Papop Lekhapanyaporn <75293379+Retaehc-pop@users.noreply.github.com>
2024-08-12 14:47:46 +0200

    change hypr config

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- move line up/down
+map("n", "<A-Down>", "<cmd>m .+1<cr>==", { desc = "Move Down" })
+map("n", "<A-Up>", "<cmd>m .-2<cr>==", { desc = "Move Up" })
+map("i", "<A-Down>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+map("i", "<A-Up>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+map("v", "<A-Down>", ":m '>+1<cr>gv=gv", { desc = "Move Down" })
+map("v", "<A-Up>", ":m '<-2<cr>gv=gv", { desc = "Move Up" })
+
+-- indentation
+map({ "n", "v" }, "<tab>", ">gv")
+map({ "n", "v" }, "<S-tab>", "<gv")
