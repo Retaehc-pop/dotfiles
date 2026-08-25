44abad33e547f714f699b8feecd0a8030fa840d6
Papop Lekhapanyaporn <75293379+Retaehc-pop@users.noreply.github.com>
2026-08-25 23:16:07 +0200

    Track caelestia, fuzzel, foot, htop, and Thunar custom actions
    Adopted from live ~/.config into the stow tree. Skipped nvtop/mpv/gtk-3.0/
    gtk-4.0/qt5ct/qt6ct (fully caelestia/noctalia-generated, no hand config)
    and Thunar/accels.scm (default Gtk accel dump).

---
+++ .config/caelestia/hypr-vars.lua
@@ -0,0 +1,9 @@
+return {
+    kbTerminal    = "SUPER + Q",   -- was SUPER + T
+    kbCloseWindow = "SUPER + C",   -- was SUPER + Q
+    kbLauncher    = "SUPER + R",   -- was SUPER + SUPER_L
+    kbEditor      = "SUPER + O",   -- bumped, C now closes windows
+    kbTodoWs      = "SUPER + Y",   -- bumped, R now opens launcher
+
+    shadowEnabled = false,         -- solid border, no glow/shadow
+}
