495ab3e201abe89b6b44c6b6e8d8fbc617b2c735
Papop Lekhapanyaporn <75293379+Retaehc-pop@users.noreply.github.com>
2026-08-26 00:46:53 +0200

    update monitor config

--- .config/caelestia/hypr-user.lua
+++ .config/caelestia/hypr-user.lua
@@ -1,3 +1,2 @@
--- eDP-1 (laptop panel) on the right, DP-4 (external) on the left, both at max refresh
 hl.monitor({ output = "DP-4", mode = "1920x1080@165", position = "0x0", scale = 1 })
-hl.monitor({ output = "eDP-1", mode = "2560x1600@165.04", position = "1920x0", scale = 1 })
+hl.monitor({ output = "eDP-1", mode = "2560x1600@165.04", position = "1920x0", scale = 1.6 })
